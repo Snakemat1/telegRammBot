e82815b63de0a911c6de788f00e7730cb3be0ff8
Adlean-Leonov <pysnakeidle@gmail.com>
2022-04-13 15:23:00 +0300

    first commit

--- const.js
+++ const.js
@@ -2,7 +2,7 @@ const commands = `
 /start - перезапустить бота
 /help - помощь
 /language - узнать о языках программирования
-
+/channelCRS - узнать о YTканалах по языкам
 `
 
 
@@ -19,9 +19,18 @@ const text3 = `
 const text4 = `
 Полный курс C++: <a href ="https://www.youtube.com/watch?v=V58rHMUVmDU">C++</a>
 `
-
-
-
+const text5 = `
+Канал по языку JavaScript <a href = "https://www.youtube.com/c/CodingTutorials/videos">JScourse</a>
+`
+const text6 = `
+Канал по языку python <a href = "https://www.youtube.com/c/PythonHubStudio">PYcourse</a>
+`
+const text7 = `
+Канал по языку Java <a href = "https://www.youtube.com/user/husivm">Jcourse</a>
+`
+const text8 = `
+Канал по языку C++ <a href = "https://www.youtube.com/c/gosha_dudar/videos">C++course</a>
+`
 
 
 module.exports.commands = commands
@@ -29,3 +38,8 @@ module.exports.text1 = text1
 module.exports.text2 = text2
 module.exports.text3 = text3
 module.exports.text4 = text4
+module.exports.text5 = text5
+module.exports.text6 = text6
+module.exports.text7 = text7
+module.exports.text8 = text8
+
